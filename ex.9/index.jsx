698c1bded4fe68f9652cc5a0fd25506291acb7fc
saulo <saulo.firmino@brilliantmachine.com.br>
2018-07-31 00:04:57 -0300

    fim de curso base sobre redux

--- ex.9/index.jsx
+++ ex.9/index.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {combineReducers, createStore} from 'redux'
 import {Provider} from 'react-redux'
-import counterReducer from './counterReducer'
-import Counter from './counter'
+import Field from './field'
+import FieldReducer from './fieldReducer'
 
 const reducers = combineReducers({
-        counter: counterReducer
+        field: FieldReducer
 })
 
 ReactDOM.render(
         <Provider store={createStore(reducers)} >
-                <Counter />
+                <Field initialValue="Teste" />
         </Provider>
 , document.getElementById('app'))
